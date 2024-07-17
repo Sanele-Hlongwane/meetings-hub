7ba4628ea9fd663e56286ae853bf9d79f62d5735
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-17 16:29:13 +0200

    Add files via upload

---
+++ constants/navbar.ts
@@ -0,0 +1,36 @@
+export const navbarLinks = [
+  {
+    imgURL: '/icons/Home.svg',
+    route: '/',
+    label: 'Home',
+  },
+
+  {
+    imgURL: '/icons/upcoming.svg',
+    route: '/about',
+    label: 'About',
+  },
+  {
+    imgURL: '/icons/previous.svg',
+    route: '/contact',
+    label: 'Contact',
+  },
+  {
+    imgURL: '/icons/Video.svg',
+    route: '/services',
+    label: 'Sevices',
+  },
+  {
+    imgURL: '/icons/add-personal.svg',
+    route: '/messages',
+    label: 'Messages',
+  },
+];
+
+export const avatarImages = [
+  '/images/avatar-1.jpeg',
+  '/images/avatar-2.jpeg',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png',
+];
