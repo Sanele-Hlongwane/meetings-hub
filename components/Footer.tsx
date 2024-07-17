4ace849b57a758e53077543a21acf78dec4a102b
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-17 19:38:10 +0200

    Fixed Footer and added some style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,31 @@
-import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import Link from 'next/link';
 
 const Footer = () => {
-
   return (
-    <footer className="bg-black text-white py-8 shadow-lg" style={{ width: '100vw' }}>
+    <footer className="bg-black text-white py-12 shadow-xl" style={{ width: '100vw' }}>
       <div className="container mx-auto text-center">
-        <p className="text-sm lg:text-base mb-4">&copy; 2024 Eagles Ring. All rights reserved.</p>
-        <p className="text-xs lg:text-sm mb-2">Contact us: contact@eaglesring.com</p>
-        <div className="text-xs lg:text-sm mb-4 flex justify-center space-x-4">
+        <div className="flex justify-between items-center mb-6">
+          <p className="text-lg lg:text-xl font-semibold">&copy; 2024 Eagles Ring. All rights reserved.</p>
+          <p className="text-sm lg:text-base">Contact us: contact@eaglesring.com</p>
+        </div>
+        <div className="text-sm lg:text-base mb-6 flex justify-center space-x-8">
           <Link href="/Terms" passHref>
             <p className="hover:text-yellow-500 transition-colors duration-300">Terms and Conditions</p>
           </Link>
         </div>
-        <div className="text-xs lg:text-sm flex justify-center items-center space-x-4">
+        <div className="text-sm lg:text-base flex justify-center items-center space-x-6">
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="ml-2">
-            <FaTwitter className="text-gray-400 hover:text-white transition-colors duration-300" />
+            <FontAwesomeIcon icon={faTwitter} className="text-gray-400 hover:text-white transition-colors duration-300" />
             <span className="sr-only">Twitter</span>
           </a>
           <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="ml-2">
-            <FaFacebook className="text-gray-400 hover:text-white transition-colors duration-300" />
+            <FontAwesomeIcon icon={faFacebook} className="text-gray-400 hover:text-white transition-colors duration-300" />
             <span className="sr-only">Facebook</span>
           </a>
           <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="ml-2">
-            <FaInstagram className="text-gray-400 hover:text-white transition-colors duration-300" />
+            <FontAwesomeIcon icon={faInstagram} className="text-gray-400 hover:text-white transition-colors duration-300" />
             <span className="sr-only">Instagram</span>
           </a>
         </div>
