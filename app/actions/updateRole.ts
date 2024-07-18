2c39f1a8ad16915e62cb3680fb3418e9dad17b77
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-18 20:53:54 +0200

    Update updateRole.ts

--- app/actions/updateRole.ts
+++ app/actions/updateRole.ts
@@ -2,7 +2,7 @@
 // UpdateRole action in backend (server-side)
 'use server';
 import { auth } from '@clerk/nextjs/server';
-import { prisma } from '@/lib/prisma';
+import prisma from '@/lib/prisma';
 
 interface RoleData {
   name: string;
@@ -29,12 +29,12 @@ async function updateRole({ name }: { name: string }): Promise<RoleResult> {
 
   try {
 
-    let role = await db.role.findFirst({
+    let role = await prisma.role.findFirst({
       where: { name: name.toLowerCase() } // Adjust for case sensitivity
     });
 
     if (!role) {
-      role = await db.role.create({
+      role = await prisma.role.create({
         data: {
           name: name.toLowerCase() // Ensure consistent casing
         }
@@ -42,7 +42,7 @@ async function updateRole({ name }: { name: string }): Promise<RoleResult> {
     }
 
 
-    const updatedUser = await db.user.update({
+    const updatedUser = await prisma.user.update({
       where: { clerkId: userId }, // Use the dynamic identifier for your user
       data: {
         roleId: role.id
