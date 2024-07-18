a0e9897bec2343654ab4e8309eff0812e1abe5bc
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-19 00:55:21 +0200

    Update page.tsx

--- app/(root)/faq/page.tsx
+++ app/(root)/faq/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const FAQ = () => {
   return (
-    <div className="p-6 bg-white dark:bg-dark-1 py-24 text-black dark:text-white">
+    <div className="p-6 bg-bg-dark-1 dark:bg-dark-1 py-24 text-white dark:text-white">
       <h1 className="text-3xl font-bold mb-4">Frequently Asked Questions (FAQ)</h1>
       
       <div className="mb-6">
