8241196438b792b269f23ccdb642f682aa88ccef
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-19 00:48:37 +0200

    Update layout.tsx

--- app/(root)/faq/layout.tsx
+++ app/(root)/faq/layout.tsx
@@ -4,7 +4,7 @@ import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 
 export const metadata: Metadata = {
-  title: 'FAQ's',
+  title: 'FAQs',
   description: 'Discover the services offered by Eagles Ring to help entrepreneurs and investors succeed.',
 };
 
