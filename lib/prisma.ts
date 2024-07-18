957717b6e00ffa4d24a10b45087aa96bb4bf0458
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-18 21:08:58 +0200

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,9 @@
-import { PrismaClient, User, Role, Entrepreneur, Investor, PrismaClientKnownRequestError } from '@prisma/client';
+import { PrismaClient, User, Role, Entrepreneur, Investor, ProEntrepreneurProfile, ProInvestorProfile, PrismaClientKnownRequestError } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export default prisma;
 
 // Exporting types explicitly using `export type`
-export type { User, Role, Entrepreneur, Investor, PrismaClientKnownRequestError };
+export type { User, Role, Entrepreneur, Investor,  ProEntrepreneurProfile, ProInvestorProfile, PrismaClientKnownRequestError };
 export { PrismaClient };
