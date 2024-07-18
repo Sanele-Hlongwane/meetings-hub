8b1b5bc607b8777707a6bbc77930aedc75207f2b
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-18 20:59:09 +0200

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,11 @@
-import { PrismaClient, User, Role, Entrepreneur, Investor, ProEntrepreneurProfile, ProInvestorProfile } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export default prisma;
-export { User, Role, Entrepreneur, Investor, ProEntrepreneurProfile, ProInvestorProfile };
+export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
-    globalThis.prisma = prisma;
+  globalThis.prisma = db;
 }
