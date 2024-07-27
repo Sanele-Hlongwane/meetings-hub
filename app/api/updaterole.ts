63692a79a412f323233979fc3a5a8f437dd25cc2
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-27 20:31:22 +0200

    Update updaterole.ts

--- app/api/updaterole.ts
+++ app/api/updaterole.ts
@@ -1,5 +1,5 @@
-// UpdateRole action in backend (server-side)
 'use server';
+
 import { auth } from '@clerk/nextjs/server';
 import { prisma } from '@/lib/prisma';
 
@@ -13,36 +13,48 @@ interface RoleResult {
 }
 
 async function updateRole({ name }: { name: string }): Promise<RoleResult> {
-  
   if (!name || name === '') {
     return { error: 'Role name is missing' };
   }
 
-
   const { userId } = auth();
-
 
   if (!userId) {
     return { error: 'User not found' };
   }
 
   try {
+    // Fetch the current user to check their existing role
+    const user = await prisma.user.findUnique({
+      where: { clerkId: userId },
+      include: { role: true },
+    });
 
+    if (!user) {
+      return { error: 'User not found' };
+    }
+
+    // Check if the user already has a role set and if it's either 'entrepreneur' or 'investor'
+    if (user.role && (user.role.name === 'entrepreneur' || user.role.name === 'investor')) {
+      return { error: 'Role cannot be changed once assigned as entrepreneur or investor' };
+    }
+
+    // Ensure the role name is in lowercase and check if it exists
     let role = await prisma.role.findFirst({
-      where: { name: name.toLowerCase() } // Adjust for case sensitivity
+      where: { name: name.toLowerCase() }
     });
 
     if (!role) {
       role = await prisma.role.create({
         data: {
-          name: name.toLowerCase() // Ensure consistent casing
+          name: name.toLowerCase()
         }
       });
     }
 
-
-    const updatedUser = await prisma.user.update({
-      where: { clerkId: userId }, // Use the dynamic identifier for your user
+    // Update the user's role
+    await prisma.user.update({
+      where: { clerkId: userId },
       data: {
         roleId: role.id
       }
