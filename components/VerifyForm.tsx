b656d5d8d017d71015a94cad0ff22d22c8525119
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-27 17:59:32 +0200

    Update VerifyForm.tsx

--- components/VerifyForm.tsx
+++ components/VerifyForm.tsx
@@ -1,40 +1,40 @@
 // app/components/VerifyForm.tsx
-import { FormEvent } from "react"
+import { FormEvent } from "react";
 
 interface VerifyFormProps {
-    handleVerify: (e: FormEvent) => void
-    code: string
-    setCode: (value: string) => void
+  handleVerify: (e: FormEvent) => void;
+  code: string;
+  setCode: (code: string) => void;
 }
 
-const VerifyForm = ({handleVerify, code, setCode}: VerifyFormProps) => {
+const VerifyForm = ({ handleVerify, code, setCode }: VerifyFormProps) => {
   return (
-    <div className="flex justify-center mt-12 grid justify-items-center md:mt-20">
+    <div className="justify-center mt-12 grid justify-items-center md:mt-20">
       <div className="h-auto bg-blue-700 rounded-xl md:rounded-3xl w-80 md:w-96">
         <div className="p-6 md:p-8">
           <h1 className="mb-6 text-3xl font-light text-white">
-            Verification Code
+            Verify Email
           </h1>
           <form onSubmit={handleVerify}>
             <input
+              className="block w-full pb-4 pl-4 mb-3 text-sm font-light bg-transparent border-0 border-b-2 h-37 border-slate-600 text-white caret-slate-700 focus:border-white"
+              placeholder="Verification Code"
+              type="text"
               value={code}
-              className="block w-full pb-4 pl-4 mb-3 text-sm font-light bg-transparent border-0 border-b-2 h-37 border-blue-900 text-white caret-slate-700 focus:border-white"
-              id="code"
-              name="code"
               onChange={(e) => setCode(e.target.value)}
+              required
             />
-
             <button
               className="w-full h-12 mb-6 text-sm font-light text-white hover:text-blue-900 hover:bg-white bg-slate-700 rounded-md"
               type="submit"
             >
-              Complete sign up
+              Complete Signup
             </button>
           </form>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default VerifyForm
+export default VerifyForm;
