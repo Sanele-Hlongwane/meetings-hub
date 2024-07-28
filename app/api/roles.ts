769aa461711a75f1f8483b6f1782b3eca46540d8
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-28 18:02:44 +0200

    Create roles.ts

---
+++ app/api/roles.ts
@@ -0,0 +1,15 @@
+// pages/api/roles.ts
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return res.status(405).end(); // Method Not Allowed
+  }
+
+  const roles = await prisma.role.findMany();
+  res.json(roles);
+}
