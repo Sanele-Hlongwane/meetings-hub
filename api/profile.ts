9e55d6bf1fb154e2f5be7a4a4110359e0a5ee2a7
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-28 19:08:53 +0200

    Update profile.ts

--- api/profile.ts
+++ api/profile.ts
@@ -1,8 +1,9 @@
-const { checkUser } = require('@/lib/checkUser');
+import { NextApiRequest, NextApiResponse } from 'next';
+const { checkUser } = require('@/lib/checkUser'); // Adjust the import path
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
-    return res.status(405).end(); 
+    return res.status(405).end(); // Method Not Allowed
   }
 
   const user = await checkUser();
