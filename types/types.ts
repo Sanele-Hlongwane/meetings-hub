7ecd5851ad12c6921272058ba2c6049053ee4f3a
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-07-28 18:09:29 +0200

    Update types.ts

--- types/types.ts
+++ types/types.ts
@@ -1,41 +1,7 @@
 // types.ts
 
-export type User = {
-    id: number;
-    clerkId: string;
-    email: string;
-    name?: string;
-    role: 'USER' | 'ENTREPRENEUR' | 'INVESTOR';
-    entrepreneur?: Entrepreneur;
-    investor?: Investor;
-    investments: Investment[];
-  };
-  
-  export type Entrepreneur = {
-    id: number;
-    userId: number;
-    user: User;
-    business: string;
-    pitch: string;
-    investments: Investment[];
-  };
-  
-  export type Investor = {
-    id: number;
-    userId: number;
-    user: User;
-    investmentOpportunities: Investment[];
-  };
-  
-  export type Investment = {
-    id: number;
-    amount: number;
-    entrepreneurId: number;
-    investorId: number;
-    entrepreneur: Entrepreneur;
-    investor: Investor;
-    user: User;
-    userId: number;
-    createdAt: Date;
-  };
-  
+import { User, Role } from '@prisma/client';
+
+export type UserWithRole = User & {
+  role: Role;
+};
